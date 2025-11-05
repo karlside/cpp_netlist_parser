960849d273ca344b7ae8941364a708d905baf86d
karlside <karlgrodaas@gmail.com>
2025-11-05 11:27:07 +0100

    Changing FSM to use switch case

--- dependencies/netlist.cpp
+++ dependencies/netlist.cpp
@@ -20,40 +20,86 @@ std::unique_ptr<std::fstream> Netlist::load_file(std::string file_path) {
   return file;
 }
 
+enum State { NEW_LINE, READ_CHAR, ADD_CHAR, ADD_WORD, ADD_LINE, DONE };
+void print_state(State state) {
+  // return;
+  switch (state) {
+  case NEW_LINE:
+    std::cout << "NEW_LINE";
+    break;
+  case READ_CHAR:
+    std::cout << "READ_CHAR";
+    break;
+  case ADD_CHAR:
+    std::cout << "ADD_CHAR";
+    break;
+  case ADD_WORD:
+    std::cout << "ADD_WORD";
+    break;
+  case ADD_LINE:
+    std::cout << "ADD_LINE";
+    break;
+  case DONE:
+    std::cout << "DONE";
+    break;
+  default:
+    std::cout << "UNKNOWN_STATE";
+    break;
+  }
+  std::cout << std::endl;
+}
+
 void Netlist::load_netlist_from_file(
     const std::unique_ptr<std::fstream> &file) {
   char ch;
   std::unique_ptr<Line> line;
   std::string word;
-  enum State { INIT, READ_CHAR, ADD_LINE };
-  State state{INIT};
-  State next_state{INIT};
+  State state{NEW_LINE};
+  State next_state{NEW_LINE};
 
-  while (true) {
-    if (INIT == state) {
+  while (State::DONE != state) {
+    // print_state(state);
+
+    switch (state) {
+
+    case State::NEW_LINE:
       line = std::make_unique<Line>();
       next_state = READ_CHAR;
-    }
+      break;
 
-    if (READ_CHAR == state) {
+    case State::READ_CHAR:
       ch = file->get();
-      if (!*file)
-        break;
-      if (!std::isspace(ch)) {
-        // std::cout << "CHAR: " << ch << std::endl;
-        word += ch;
+      if (!*file) {
+        next_state = DONE;
+      } else if (!std::isspace(ch)) {
+        next_state = ADD_CHAR;
       } else {
-        line->add(word);
-        word = "";
-        if ('\n' == ch) {
-          next_state = ADD_LINE;
-        }
+        next_state = ADD_WORD;
       }
-    }
+      break;
 
-    if (ADD_LINE == state) {
+    case State::ADD_CHAR:
+      word += ch;
+      next_state = READ_CHAR;
+      break;
+
+    case State::ADD_WORD:
+      line->add(word);
+      word = "";
+      if ('\n' == ch) {
+        next_state = ADD_LINE;
+      } else {
+        next_state = READ_CHAR;
+      }
+      break;
+
+    case State::ADD_LINE:
       add_line(std::move(line));
-      next_state = INIT;
+      next_state = NEW_LINE;
+      break;
+
+    case State::DONE:
+      break;
     }
 
     state = next_state;
