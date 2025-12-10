994f844253939a1f9becfa51208b9f3b7ad01aba
karlside <karlgrodaas@gmail.com>
2025-12-10 10:23:03 +0100

    Deleting comments

--- dependencies/netlist.h
+++ dependencies/netlist.h
@@ -16,38 +16,6 @@ struct ListOfLines : ListOfTypes<Statement> {
   }
 };
 
-// struct LineEntry {
-//   LineEntry(std::pair<std::string, std::shared_ptr<Statement>> input)
-//       : key{input.first}, line{input.second} {}
-//
-//   std::string const key;
-//   std::shared_ptr<Statement> line;
-//   std::string print_line() {
-//     return "ID:'" + key + "' - " + line->print_line();
-//   }
-// };
-//
-// struct ListOfLines {
-// public:
-//   void push_back(std::shared_ptr<Statement> input);
-//   std::shared_ptr<Statement> get_word(std::string key);
-//   std::shared_ptr<Statement> pop_back();
-//   // std::shared_ptr<StatementWord> at(int index) { return words.at(index); }
-//   int size() { return lines.size(); }
-//
-//   std::vector<LineEntry> lines;
-//
-//   friend std::ostream &operator<<(std::ostream &os, const ListOfLines &rhs);
-//
-// private:
-//   std::unordered_map<std::string, int> index;
-//   std::string create_key(std::string key, int iterator = 0);
-// };
-
-// ---------------
-// --- Netlist ---
-// ---------------
-
 class Netlist {
 public:
   void add_line(std::shared_ptr<Statement> line);
