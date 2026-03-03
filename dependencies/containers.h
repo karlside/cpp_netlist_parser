5c7d10529768776638f178779ddd5c406f73915b
karlside <karlgrodaas@gmail.com>
2026-03-03 15:48:59 +0100

    Fixed minor bug in contanmiers.h

--- dependencies/containers.h
+++ dependencies/containers.h
@@ -86,10 +86,7 @@ template <typename StatementType>
 std::shared_ptr<StatementType> ListOfTypes<StatementType>::pop_back() {
   Item ret_item = items.back();
   items.pop_back();
-  index.erase(
-      ret_item.item->get_text()); // TODO: groc says there is a bug here. It
-                                  // says it should use ret_item.key
-                                  // istead of ret_item.time-get_text()
+  index.erase(ret_item.key);
   return ret_item.item;
 }
 
